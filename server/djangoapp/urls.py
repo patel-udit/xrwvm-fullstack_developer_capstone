9a96d6c9dba998ca15ab8762da8bfd4f39f69b54
Udit Patel <uditpatel0830@gmail.com>
2024-07-18 18:04:45 -0400

    Created carmodel and carmake

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -12,6 +12,7 @@ urlpatterns = [
     path(route='login', view=views.login_user, name='login'),
     path('logout', views.logout_request, name='logout_request'),
     path('register', views.registration, name='registration'),
+        path(route='get_cars', view=views.get_cars, name ='getcars'),
     # path for dealer reviews view
 
     # path for add a review view
