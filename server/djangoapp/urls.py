55a4a659ba73fc69a2952e2473c91f772996c241
Udit Patel <uditpatel0830@gmail.com>
2024-07-19 17:55:12 -0400

    half way there

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -9,17 +9,19 @@ urlpatterns = [
     # # path for registration
 
     # path for login
-    path(route='login', view=views.login_user, name='login'),
-    path('logout', views.logout_request, name='logout_request'),
+    path(route='login', view=views.login_user, name='login'),  
+    path(route='logout', view=views.logout_request, name='logout'), 
+    path(route='registration', view=views.registration, name='registration'),
+
     path('register', views.registration, name='registration'),
-        path(route='get_cars', view=views.get_cars, name ='getcars'),
+    path(route='get_cars', view=views.get_cars, name ='getcars'),
     # path for dealer reviews view
-path(route='get_dealers', view=views.get_dealerships, name='get_dealers'),
+    path(route='get_dealers', view=views.get_dealerships, name='get_dealers'),
     path(route='get_dealers/<str:state>', view=views.get_dealerships, name='get_dealers_by_state'),
-        path(route='dealer/<int:dealer_id>', view=views.get_dealer_details, name='dealer_details'),
+    path(route='dealer/<int:dealer_id>', view=views.get_dealer_details, name='dealer_details'),
     # path for add a review view
     path(route='reviews/dealer/<int:dealer_id>', view=views.get_dealer_reviews, name='dealer_details'),
-        path(route='add_review', view=views.add_review, name='add_review'),
+    path(route='add_review', view=views.add_review, name='add_review'),
 
         
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
