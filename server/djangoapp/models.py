1fd97b29eb3e6b9452abdb09a084b821863b4850
Udit Patel <uditpatel0830@gmail.com>
2024-07-31 17:06:11 -0400

    update formatting

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.utils.timezone import now
+# from django.utils.timezone import now
 from django.core.validators import MaxValueValidator, MinValueValidator
 
 
@@ -16,6 +16,7 @@ class CarMake(models.Model):
 
     def __str__(self):
         return self.name
+
 
 # <HINT> Create a Car Model model `class CarModel(models.Model):`:
 # - Many-To-One relationship to Car Make model (One Car Make has many
@@ -40,6 +41,7 @@ class CarModel(models.Model):
             MaxValueValidator(2023),
             MinValueValidator(2015)
         ])
+
 
     def __str__(self):
         return self.name
