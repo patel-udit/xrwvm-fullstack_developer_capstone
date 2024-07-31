3e33b718df9f18c12c359d9346ef914d2a045cf0
Udit Patel <uditpatel0830@gmail.com>
2024-07-31 17:37:16 -0400

    lint python files 2.0

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -29,9 +29,9 @@ SECRET_KEY =\
 DEBUG = True
 
 ALLOWED_HOSTS = ['localhost', 'https://uditpatel083-8000.theianext-1' +
-               '-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai']
+                 '-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai']
 CSRF_TRUSTED_ORIGINS = ['https://uditpatel083-8000.theianext-1-labs-' +
-                      'prod-misc-tools-us-east-0.proxy.cognitiveclass.ai']
+                        'prod-misc-tools-us-east-0.proxy.cognitiveclass.ai']
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [],
